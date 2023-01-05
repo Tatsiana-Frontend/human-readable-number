b80a736307ef64080b7eb5be25601c2570ea77cc
Tatsiana Artuganova <artuganova85@gmail.com>
2023-01-05 13:56:43 +0300

    feat: add function toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function toReadable (number) {
-  
-}
+    const units = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
+    const tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
+    const digit = number%10;
+
+    if (number < 20) {
+        return units[number];
+    } else if (number < 100) {
+        return tens[Math.floor(number/10)-2] + (digit ? (" " + units[digit]) : "");
+    } else {
+        return units[Math.floor(number/100)] +" hundred" + (number%100 == 0 ? "" : " " + toReadable(number%100));
+    }
+};
